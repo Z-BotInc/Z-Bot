fe18afdcf81d111336827ae7d474adfe5c02304d
Z-BotInc <62592978+Z-BotInc@users.noreply.github.com>
2020-03-26 16:20:46 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,10 +3,11 @@ const {table} = require('table');
 const figlet = require('figlet');
 
 /**
- * 
+ * Ready Event
  * 
  * @param {Discord.Client} bot
  */
 module.exports = (bot) => {
-
+    figlet("I'm ready", (err, data) => err ? console.log(err) : console.log(data));
+    bot.user.setActivity(`${bot.guilds.cache.size} serveur(s) | ${bot.config.prefix}help`, { type: "WATCHING" });
 };
